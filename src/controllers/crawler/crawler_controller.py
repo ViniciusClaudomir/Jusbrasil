fd79ee48539ee4c8586f516244dc952d1566f86d
Vinicius Claudomir do Carmo Silva <vinicius.silva@parvi.com.br>
2022-03-16 11:31:19 -0300

    feat: create adapter to vultr page

--- src/controllers/crawler/crawler_controller.py
+++ src/controllers/crawler/crawler_controller.py
@@ -1,12 +1,22 @@
 from typing import Any
 import requests as r
+from bs4 import BeautifulSoup as bs
 
-class CrawlerController(): 
-	def __init__(self) -> None:
-		pass
-
+class CrawlerController():
+    
+	def __init__(self, crawlerAdapter) -> None:
+		self.crawlerAdapter = crawlerAdapter()
+    
 	def handle(self, url: str) -> Any:
-		
-		response = r.get(url)
-		if response.status_code == 200:
-			return True
+		try:
+			response = r.get(url)
+			if response.status_code != 200:
+				return False
+
+			page = bs(response.text, 'html')
+			self.dict_page = self.crawlerAdapter.to_dict(page)
+            
+
+		except Exception as e:
+			print(e)
+			return False
