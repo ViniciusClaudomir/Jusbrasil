bd95759995d129c422a25fde8dff0dc78a806c18
Vinicius Claudomir do Carmo Silva <vinicius.silva@parvi.com.br>
2022-03-17 12:39:59 -0300

    refactor: move render page to adapters

--- src/controllers/crawler/crawler_controller.py
+++ src/controllers/crawler/crawler_controller.py
@@ -3,31 +3,58 @@ import requests as r
 from bs4 import BeautifulSoup as bs
 import json
 from src.controllers.helpers.print_console import print_table
-class CrawlerController():
-    
-	def __init__(self, crawlerAdapter) -> None:
-		self.crawlerAdapter = crawlerAdapter()
-    
-	def handle(self, url: str) -> Any:
-		try:
-			response = r.get(url)
-			if response.status_code != 200:
-				return False
+from src.controllers.helpers.save_csv import to_csv
 
-			page = bs(response.text, 'html')
-			self.dict_page = self.crawlerAdapter.to_dict(page)
+'''
+Ao importar o adapter o mesmo deve ter um metodo chamado to_dict onde deve retornar no formato
+
+{
+    0: {
+        'title':'Titulo Qualquer',
+        'data': {
+            0: {
             
-		except Exception as e:
-			print(e)
-			return False
+            }, ...
         
-	def to_json(self, file_name: str) -> Any:
-		if file_name.endswith(".json"):
-			with open(file_name, 'w') as arq:
-				arq.write(json.dumps(self.dict_page))
-		else:
-			raise Exception("Prefix json error")
-			
-	@property
-	def console(self):
-		print_table(self.dict_page)
+        }
+    }, ...
+
+
+}
+
+'''
+
+class CrawlerController():
+
+    def __init__(self, crawlerAdapter) -> None:
+        self.crawlerAdapter = crawlerAdapter()
+
+    def handle(self, url: str) -> Any:
+        try:
+            response = r.get(url)
+            if response.status_code != 200:
+                return False
+
+            self.dict_page = self.crawlerAdapter.to_dict(url)
+
+        except Exception as e:
+            print(e)
+            return False
+
+    def to_json(self, file_name: str) -> Any:
+        if file_name.endswith(".json"):
+            with open(file_name, 'w') as arq:
+                arq.write(json.dumps(self.dict_page))
+        else:
+            raise Exception("Prefix json error")
+            
+    def to_csv(self, file_name: str) -> Any:
+        if file_name.endswith(".csv"):
+            with open(file_name, 'w') as arq:
+                csv = to_csv(self.dict_page)
+                arq.write(csv)
+        else:
+            raise Exception("Prefix csv error")
+        
+    def console(self):
+        print_table(self.dict_page)
